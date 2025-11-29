241f18d6bfeae034228f25f79dc8fd2659393f76
Javohir-042 <javohirquromboyev933@gmail.com>
2025-11-29 18:04:58 +0500

    O'zgartirishlar kiritildi

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import qidiruv from "./assets/icons/qidiruv.svg";
 import like from "./assets/icons/like.svg";
 import compare from "./assets/icons/1.svg";
 import cart from "./assets/icons/2.svg";
+import logo2 from "./assets/icons/logo2.svg";
+
+import visa from "./assets/png/11.png";
 
 const Company = lazy(() => import("./pages/Company"));
 const Delivery = lazy(() => import("./pages/Delivery"));
@@ -57,7 +60,7 @@ function App() {
               <div className="head_desc">
                 <div className="head_norlignt">
                   <Link to="/company">
-                    <img src={logo} alt="logo" />
+                    <img className="logo_img" src={logo} alt="logo" />
                   </Link>
                 </div>
 
@@ -80,9 +83,13 @@ function App() {
                   </div>
 
                   <div className="head_svg1">
-                    <img src={cart} alt="cart" />
+                    <img className="img_shox" src={cart} alt="cart" />
                   </div>
                 </div>
+              </div>
+              <div className="head_input head_input2">
+                <input type="text" placeholder="Поиск по товарам" />
+                <img src={qidiruv} alt="search" />
               </div>
             </div>
           </header>
@@ -98,6 +105,56 @@ function App() {
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/blog" element={<Blog />} />
         </Routes>
+
+        <footer className="footer_blog">
+          <div className="container">
+            <div className="footer_text_2">
+              <div className="footer_logo">
+                <img src={logo2} alt="" className="footer_img_1" />
+                <p className="footer_number">8 (800) 890-46-56</p>
+                <img src={visa} alt="" />
+                <p className="footer_text">Политика конфидециальности</p>
+                <p className="footer_text">Пользовательское соглашение</p>
+                <div className="footer_vk">
+                  <p>VK</p>
+                  <p>VK</p>
+                  <p>VK</p>
+                </div>
+              </div>
+              <div className="footer_h1">
+                <h1>Покупателям</h1>
+                <div className="footer_text_3">
+                  <p>O компании</p>
+                  <p>Доставка и оплата</p>
+                  <p>Возврат</p>
+                  <p>Гарантии</p>
+                  <p>Контакты</p>
+                  <p>Блог</p>
+                </div>
+              </div>
+              <div>
+                <h1>Товары</h1>
+                <div className="footer_text_3">
+                  <p>Люстры</p>
+                  <p>Светильники</p>
+                  <p>Бра</p>
+                  <p>Торшеры</p>
+                  <p>Комплектуюшие</p>
+                  <p>Настольные лампы</p>
+                </div>
+              </div>
+              <div>
+                <div className="footer_text_33">
+                  <p>Споты</p>
+                  <p>Трековые светильники</p>
+                  <p>Уличные светильники</p>
+                  <p>Технические светильники</p>
+                  <p>Светодиодные ленты</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
       </Suspense>
     </BrowserRouter>
   );
--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+import left from "../assets/icons/3.svg";
+import rght from "../assets/icons/4.svg";
+import right_ from "../assets/icons/5.svg";
+import img from "../assets/icons/6.svg";
+
+import arter from "../assets/png/5.png";
+import divinare from "../assets/png/6.png";
+import odion from "../assets/png/7.png";
+import dom1 from "../assets/png/8.png";
+
 const Company = () => {
   return (
     <div className="container">
@@ -27,7 +37,7 @@ const Company = () => {
             </div>
           </div>
         </div>
-        <div className="company_desc">
+        <div className="company_desc ">
           <p>
             Интернет-магазин NORNLIGHT предлагает широкий ассортимент
             светильников для освещения вашего дома или офиса. У нас вы найдете
@@ -61,6 +71,63 @@ const Company = () => {
             удобством покупки онлайн. Обратитесь к нам сегодня и превратите ваш
             дом в оазис тепла и света с NORNLIGHT!
           </p>
+        </div>
+      </div>
+      <div className="company_title">
+        <div>
+          <h1>Только проверенные бренды</h1>
+        </div>
+        <div>
+          <img src={left} alt="" />
+          <img src={rght} alt="" />
+        </div>
+      </div>
+
+      <div className="company_png">
+        <img src={arter} alt="" />
+        <img src={divinare} alt="" />
+        <img src={odion} alt="" />
+        <img src={arter} alt="" />
+        <img src={divinare} alt="" />
+      </div>
+
+      <div className="company_blog">
+        <div>
+          <h1>Блог</h1>
+        </div>
+        <div>
+          <button className="company_bottom">
+            Перейти в блог <img src={right_} alt="" />
+          </button>
+        </div>
+      </div>
+
+      <div className="company_cardblog">
+        <div className="company_card">
+          <img src={dom1} alt="" />
+          <div className="company_subtitle">
+            <p>Как правильно освещать дом снаружи?</p>
+            <img src={img} alt="" />
+          </div>
+          <p>01.01.2024</p>
+        </div>
+
+        <div className="company_card">
+          <img src={dom1} alt="" />
+          <div className="company_subtitle">
+            <p>Как правильно освещать дом снаружи?</p>
+            <img src={img} alt="" />
+          </div>
+          <p>01.01.2024</p>
+        </div>
+
+        <div className="company_card">
+          <img src={dom1} alt="" />
+          <div className="company_subtitle">
+            <p>Как правильно освещать дом снаружи?</p>
+            <img src={img} alt="" />
+          </div>
+          <p>01.01.2024</p>
         </div>
       </div>
     </div>
